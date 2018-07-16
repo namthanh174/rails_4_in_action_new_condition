80ae25d0c970d77e098710f0fa700087bccc90ee
thanh vo <namthanh174@gmail.com>
2018-07-16 08:29:39 +0000

    Display a comment's state transistion'

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,10 @@ end
 ["Sublime Text 3", "Internet Explorer"].each do |name|
   Project.create!(name: name, description: "A sample project about #{name}")
 end
+
+unless State.exists?
+  State.create(name: "New", color: "#0066CC")
+  State.create(name: "Open", color: "#008000")
+  State.create(name: "Closed", color: "#990000")
+  State.create(name: "Awesome", color: "#663399")
+end
