cf231186e2b047cfa0048b0772708a82f0419786
namthanh174 <namthanh174@gmail.com>
2018-07-19 10:45:32 +0700

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
 
-  host = "thanh-ticketee.herokuapp.com"
+  host = "thanh-ticketee.com"
   
   ActionMailer::Base.smtp_settings = {
     port:           ENV['MAILGUN_SMTP_PORT'],
